f7754322964de860ccf79fd606e84510fa616c12
lailahaidar <145322841+lailahaidar@users.noreply.github.com>
2023-11-09 14:57:52 +0300

    Laila's work

--- poll_project/poll/admin.py
+++ poll_project/poll/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Poll
+from .models import Option
+from .models import Response
+from django.db.models import Count
+
+class OptionInLine(admin.TabularInline):
+    model = Option
+    extra = 0
+    classes = ('collapse',)
+
+class ResponseInLine(admin.TabularInline):
+    model = Response
+    extra = 0
+    classes = ('collapse',)
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question','active_until','status','response_count']
+    list_filter = ['status']
+    search_fields = ['question']
+    inlines = [OptionInLine,ResponseInLine,]
+
+    def response_count(self, obj):
+        return obj.option_set.aggregate(response_count=Count('response')).get('response_count', 0)  
+        #we use .aggregate(response_count=Count('response')): to count respond
+    
+    pass
+
+class OptionAdmin(admin.ModelAdmin):
+    inlines = [ResponseInLine,]
+    pass
+
+class ResponseAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.site_header="This is our Poll Project"
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Option, OptionAdmin)
+admin.site.register(Response, ResponseAdmin)
--- poll_project/poll/models.py
+++ poll_project/poll/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Poll(models.Model):
+  title = models.CharField(max_length=200, unique=True)
+  question = models.TextField()
+  active_until = models.DateTimeField(auto_now_add=False)
+  status = models.IntegerField(default=0)
+
+class Option(models.Model):
+  title = models.CharField(max_length=200, unique=True)
+  poll = models.ForeignKey('Poll', on_delete=models.CASCADE)
+
+class Response(models.Model):
+  name = models.CharField(max_length=200, unique=False)
+  response_time = models.DateTimeField(auto_now_add=True)
+  option = models.ForeignKey('Option', on_delete=models.CASCADE)
+  poll = models.ForeignKey('Poll', on_delete=models.CASCADE)
